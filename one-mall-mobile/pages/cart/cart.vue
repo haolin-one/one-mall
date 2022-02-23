f3d48f59e690c56d7edc5d30364559f63cf0af79
howLing-One <1393452797@qq.com>
2022-02-23 23:53:30 +0800

    chore(one-mall-mobile): initialize the uniapp project
    Initialize the uniapp project

---
+++ one-mall-mobile/pages/cart/cart.vue
@@ -0,0 +1,22 @@
+<template>
+  <view>
+    我是购物车
+  </view>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        
+      }
+    },
+    methods: {
+      
+    }
+  }
+</script>
+
+<style>
+
+</style>
---
+++ one-mall-mobile/pages/cate/cate.vue
@@ -0,0 +1,22 @@
+<template>
+  <view>
+    我是分类
+  </view>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        
+      }
+    },
+    methods: {
+      
+    }
+  }
+</script>
+
+<style>
+
+</style>
